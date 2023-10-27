360b7dab30385382e61e7e1da0af1376638ffa99
prajnavohraa <prajna21345@iiitd.ac.in>
2023-10-27 18:12:30 +0530

    etl updated

--- etl.py
+++ etl.py
@@ -1,18 +1,3 @@
-# import pandas as pd;
-# from sqlalchemy import create_engine
-# import os
-
-# #EXTRACT
-# original_rainfall_csv="iia\originaldatasets\original_rainfall.csv"
-# original_rainfall_csv_data = pd.read_csv(original_rainfall_csv)
-# # print(original_rainfall_csv_data.head())
-# # print(original_rainfall_csv_data.dtypes)
-
-# #TRANSFORM
-
-# original_rainfall_csv_data.drop(columns=["JF","MAM","JJAS","OND"])
-# original_rainfall_csv_data.rename(columns={"SUBDIVISION":"State"})
-# original_rainfall_csv_data.to_csv("iia\\transformeddatasets\\transformed_rainfall.csv")
 
 import pandas as pd
 import os
